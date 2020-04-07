fdd63c3630da681dc4cb906698737454f83e970f
Lycanite <rjnicholson1591@gmail.com>
2020-04-07 21:54:00 +0100

    Royal Archvile Altar

--- src/main/java/com/lycanitesmobs/core/info/AltarInfo.java
+++ src/main/java/com/lycanitesmobs/core/info/AltarInfo.java
@@ -117,6 +117,9 @@ public class AltarInfo {
 
         AltarInfo crimsonEpionAltar = new AltarInfoCrimsonEpion("CrimsonEpion");
         addAltar(crimsonEpionAltar);
+
+        AltarInfo royalArchvileAltar = new AltarInfoRoyalArchvile("RoyalArchvile");
+        addAltar(royalArchvileAltar);
     }
 
 
