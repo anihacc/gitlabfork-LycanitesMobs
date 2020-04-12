d28dd751f3e6a30e22c134a2af81c452ecf59aea
Lycanite <rjnicholson1591@gmail.com>
2020-04-12 15:31:27 +0100

    Dungeon Locate Command New Command Added: /lm dungeons locate

--- src/main/java/com/lycanitesmobs/core/command/DungeonsCommand.java
+++ src/main/java/com/lycanitesmobs/core/command/DungeonsCommand.java
@@ -1,19 +1,28 @@
 package com.lycanitesmobs.core.command;
 
+import com.lycanitesmobs.ExtendedWorld;
 import com.lycanitesmobs.core.config.ConfigDungeons;
 import com.lycanitesmobs.core.dungeon.DungeonManager;
+import com.lycanitesmobs.core.dungeon.instance.DungeonInstance;
 import com.mojang.brigadier.builder.ArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+
+import java.util.List;
 
 public class DungeonsCommand {
 	public static ArgumentBuilder<CommandSource, ?> register() {
 		return Commands.literal("dungeons")
 				.then(Commands.literal("reload").executes(DungeonsCommand::reload))
 				.then(Commands.literal("enable").executes(DungeonsCommand::enable))
-				.then(Commands.literal("disable").executes(DungeonsCommand::disable));
+				.then(Commands.literal("disable").executes(DungeonsCommand::disable))
+				.then(Commands.literal("locate").executes(DungeonsCommand::locate));
 	}
 
 	public static int reload(final CommandContext<CommandSource> context) {
@@ -33,6 +42,21 @@ public class DungeonsCommand {
 		ConfigDungeons.INSTANCE.dungeonsEnabled.set(false);
 		ConfigDungeons.INSTANCE.dungeonsEnabled.save();
 		context.getSource().sendFeedback(new TranslationTextComponent("lyc.command.dungeons.disable"), true);
+		return 0;
+	}
+
+	public static int locate(final CommandContext<CommandSource> context) {
+		context.getSource().sendFeedback(new TranslationTextComponent("lyc.command.dungeons.locate"), true);
+		World world = context.getSource().getWorld();
+		ExtendedWorld extendedWorld = ExtendedWorld.getForWorld(world);
+		List<DungeonInstance> nearbyDungeons = extendedWorld.getNearbyDungeonInstances(new ChunkPos(new BlockPos(context.getSource().getPos())), ConfigDungeons.INSTANCE.dungeonDistance.get() * 2);
+		if(nearbyDungeons.isEmpty()) {
+			context.getSource().sendFeedback(new TranslationTextComponent("common.none"), true);
+			return 0;
+		}
+		for(DungeonInstance dungeonInstance : nearbyDungeons) {
+			context.getSource().sendFeedback(new StringTextComponent(dungeonInstance.toString()), true);
+		}
 		return 0;
 	}
 }
