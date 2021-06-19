03087e997a9fa591c82ec54c67b937e027888659
Richard Nicholson <richard@wearearch.com>
2021-06-19 15:55:17 +0100

    Removed Pet Follow Speed Boost

--- src/main/java/com/lycanitesmobs/core/entity/TameableCreatureEntity.java
+++ src/main/java/com/lycanitesmobs/core/entity/TameableCreatureEntity.java
@@ -108,7 +108,7 @@ public abstract class TameableCreatureEntity extends AgeableCreatureEntity {
 		this.targetSelector.addGoal(this.nextSpecialTargetIndex++, new DefendOwnerGoal(this));
 
 		// Lesser Actions:
-		this.goalSelector.addGoal(this.nextTravelGoalIndex++, new FollowOwnerGoal(this).setStrayDistance(CreatureManager.getInstance().config.petFollowDistance).setLostDistance(32).setSpeed(1.5D));
+		this.goalSelector.addGoal(this.nextTravelGoalIndex++, new FollowOwnerGoal(this).setStrayDistance(CreatureManager.getInstance().config.petFollowDistance).setLostDistance(32).setSpeed(1D));
 		this.goalSelector.addGoal(this.nextIdleGoalIndex++, new BegGoal(this));
     }
     
