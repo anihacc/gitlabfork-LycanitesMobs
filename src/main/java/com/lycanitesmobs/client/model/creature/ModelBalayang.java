a10b9054cc486b8515afd90b5f211fcb5f2eb6ad
TehMadTitan <ianonymousarmy@gmail.com>
2021-06-25 19:57:38 -0400

    balayang to 1.12.2 WIP

--- src/main/java/com/lycanitesmobs/client/model/creature/ModelBalayang.java
+++ src/main/java/com/lycanitesmobs/client/model/creature/ModelBalayang.java
@@ -1,15 +1,14 @@
 package com.lycanitesmobs.client.model.creature;
 
+
 import com.lycanitesmobs.LycanitesMobs;
+import com.lycanitesmobs.client.model.ModelObjOld;
 import com.lycanitesmobs.client.model.template.ModelTemplateBiped;
-
-
+import com.lycanitesmobs.core.entity.BaseCreatureEntity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-
-
 
 
 @SideOnly(Side.CLIENT)
@@ -27,6 +26,7 @@ public class ModelBalayang extends ModelTemplateBiped {
 	public void animatePart(String partName, EntityLiving entity, float time, float distance, float loop, float lookY, float lookX, float scale) {
 		super.animatePart(partName, entity, time, distance, loop, lookY, lookX, scale);
 		if (entity != null && (entity.onGround || entity.isInWater())) {
+
 			if (partName.equals("wingleft01")) {
 				float rotX = -40;
 				float rotY = 20.5F;
