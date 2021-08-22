87db8f0bab6aa9ee5e972508b9c36f1127c87571
Richard Nicholson <richard@wearearch.com>
2021-08-22 12:32:42 +0100

    Cap Child projectile Fire Sound to Once Per Second

--- src/main/java/com/lycanitesmobs/core/info/projectile/behaviours/ProjectileBehaviourFireProjectiles.java
+++ src/main/java/com/lycanitesmobs/core/info/projectile/behaviours/ProjectileBehaviourFireProjectiles.java
@@ -131,7 +131,9 @@ public class ProjectileBehaviourFireProjectiles extends ProjectileBehaviour {
 				0
 		);
 
-		projectile.playSound(childProjectile.getLaunchSound(), 1.0F, 1.0F / (projectile.getCommandSenderWorld().getRandom().nextFloat() * 0.4F + 0.8F));
+		if (projectile.projectileLife % 20 == 0) {
+			projectile.playSound(childProjectile.getLaunchSound(), 1.0F, 1.0F / (projectile.getCommandSenderWorld().getRandom().nextFloat() * 0.4F + 0.8F));
+		}
 		projectile.getCommandSenderWorld().addFreshEntity(childProjectile);
 
 		return childProjectile;
