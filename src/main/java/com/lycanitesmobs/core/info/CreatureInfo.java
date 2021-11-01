2c3a4aa02dac956f77e1f12c241fcb9ce3feb9f4
Richard Nicholson <richard@wearearch.com>
2021-11-01 11:38:17 +0000

    Added special exception for creature config loading issues with instructions to delete old configs when updated from an older version of the mod. Updated CreatureInfo, tags are now cheked using the contains method of the tag instance rather than an is method on the item instance.

--- src/main/java/com/lycanitesmobs/core/info/CreatureInfo.java
+++ src/main/java/com/lycanitesmobs/core/info/CreatureInfo.java
@@ -196,7 +196,7 @@ public class CreatureInfo {
 		}
 		catch(Exception e) {
 			LycanitesMobs.logError("[Creature] Unable to find the Java Entity Class: " + json.get("entityClass").getAsString() + " for " + this.getName());
-			throw new RuntimeException(e);
+			throw new RuntimeException("Creature config loading error, clear your creatures config if updating from an older version of the mod. Missing Class: " + json.get("entityClass").getAsString() + " Creature: " + this.getName());
 		}
 
 		// Enabled:
@@ -823,7 +823,7 @@ public class CreatureInfo {
 				LycanitesMobs.logWarning("", "[Creature] Cannot find diet: " + dietTagId);
 				return false;
 			}
-			if(itemStack.getItem().is(dietTag)) {
+			if(dietTag.contains(itemStack.getItem())) {
 				return true;
 			}
 		}
